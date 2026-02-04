9cd0232e257cc62f3fc9f07eeee26d7cf46f8640
kaladoo <144223697+kaladoodotlua@users.noreply.github.com>
2026-02-04 21:00:17 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+echo -e "\nChecking for required commands...\n"
+
+if command -v git >/dev/null 2>&1; then
+    echo -e "\e[32mGit installed!\e[0m"
+else
+    echo -e "\e[31mGit is not installed. Please install it.\e[0m"
+    exit 1
+fi
+
+if command -v lua >/dev/null 2>&1; then
+    echo -e "\e[32mLua installed!\e[0m"
+else
+    echo -e "\e[31mLua is not installed. Please install it.\e[0m"
+    exit 1
+fi
+
+INSTALL_DIR="/opt/LRhub"
+VERSION=1.3
+
+echo -e "\nCloning LRhub into $INSTALL_DIR..."
+
+if [ -d "$INSTALL_DIR" ]; then
+    echo -e "\e[33mExisting LRhub directory found. Removing...\e[0m"
+    sudo rm -rf "$INSTALL_DIR"
+fi
+
+sudo git clone https://github.com/kaladoodotlua/LRhub.git "$INSTALL_DIR"
+
+echo -e "\nCreating run script..."
+
+sudo tee /usr/bin/lrhub >/dev/null << EOF
+#!/bin/bash
+cd "$INSTALL_DIR" || exit 1
+lua hub.lua "\$@"
+EOF
+
+sudo chmod +x "/usr/bin/lrhub"
+
+echo -e "\n\e[32mLRhub installed successfully!\e[0m"
+echo -e "Cleaning up...\n"
+sudo mv $INSTALL_DIR/LRhub-v$VERSION/tools $INSTALL_DIR
+sudo mv $INSTALL_DIR/LRhub-v$VERSION/hub.lua $INSTALL_DIR
+sudo rm -r $INSTALL_DIR/LRhub-v$VERSION
+
+echo -e "Run with:\n\e[33mlrhub\e[0m"
